258a6d8f83b81819cdea0e9604c80b107094ce9a
Kuniaki Igarashi <igaiga@gmail.com>
2020-07-21 18:14:35 +0900

    Add comment outed cp932 encoding code

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   def self.generate_csv
     csv_attributes = ["title", "author"]
+#    CSV.generate(headers: true, encoding: "cp932") do |csv|
     CSV.generate(headers: true) do |csv|
       csv << csv_attributes
       self.all.each do |book|
